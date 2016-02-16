810a3aa591b522b8fab7b15743c18945da43120a
Виталий <virtuos86@yandex.ru>
2016-02-16 13:49:05 +0600

    Стартовый коммит. Добро пожаловать в мир Величайшей Игры :-).

---
+++ const.py
@@ -0,0 +1,66 @@
+# coding: utf-8
+
+"""
+const.py - константы игры (по сути, конфиг)
+"""
+
+from kivy import platform as _platform
+
+###############################################################################
+
+class Colors:
+    blue   = (0, 0, 1, 1)
+    gray   = (0.5, 0.5, 0.5, 1)
+    green  = (0, 0.5, 0, 1)
+    lime   = (0, 1, 0, 1)
+    maroon = (0.5, 0, 0, 1)
+    navy   = (0, 0, 0.5, 1)
+    purple = (0.5, 0, 0.5, 1)
+    red    = (1, 0, 0, 1)
+    silver = (.75, .75, .75, 1)
+    teal   = (0, 0.5, 0.5, 1)
+
+# Ширина игрового поля относительно ширины всего экрана.
+AREA_WIDTH = 0.7
+# Высота игрового поля относительно высоты всего экрана.
+AREA_HEIGHT = 1
+
+if _platform == 'android':
+	# ширина видимой области
+	LOCAL_MAP_WIDTH = 8
+	# высота видимой области
+	LOCAL_MAP_HEIGHT = 8
+
+	# ширина карты
+	MAP_WIDTH = 32 + LOCAL_MAP_WIDTH * 2
+	# высота карты
+	MAP_HEIGHT = 32 + LOCAL_MAP_HEIGHT * 2
+else:
+	LOCAL_MAP_WIDTH = 20
+	LOCAL_MAP_HEIGHT = 20
+
+	MAP_WIDTH = 80 + LOCAL_MAP_WIDTH * 2
+	MAP_HEIGHT = 80 + LOCAL_MAP_HEIGHT * 2
+
+# Начало окна вывода видимой части карты (оставлено для совместимости).
+WINDOW_LEFT = 0
+WINDOW_TOP = 0
+
+###############################################################################
+
+# проходимые тайлы
+tileGrass = 1;
+tileGround = 2;
+tileStairsUp = 3;
+tileStairsDown = 4;
+
+# непроходимые тайлы
+tileFirstStopTile = 5;
+tileTree = tileFirstStopTile;
+tileStone = tileFirstStopTile + 1;
+tileLast = tileFirstStopTile + 1
+
+###############################################################################
+
+# глубина пещеры
+MaxDungeonLevel = 7
---
+++ main.py
@@ -0,0 +1,8 @@
+import map
+
+def main():
+	map.MapGeneration(1)
+	map.ShowMap()
+
+if __name__ == '__main__':
+	main()
